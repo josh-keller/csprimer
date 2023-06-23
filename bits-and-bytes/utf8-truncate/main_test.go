35913ad40d89cfaaba793c30a5fc3c4e0fa355de
Josh Keller <jrkeller@gmail.com>
2023-06-23 07:50:20 -0400

    Update utf8 encode

--- bits-and-bytes/utf8-truncate/main_test.go
+++ bits-and-bytes/utf8-truncate/main_test.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"bufio"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestTruncate(t *testing.T) {
+	cases, err := os.Open("./cases")
+	must(err)
+	expected, err := os.Open("./expected")
+	must(err)
+	caseScanner := bufio.NewScanner(cases)
+	expScanner := bufio.NewScanner(expected)
+
+	for caseScanner.Scan() && expScanner.Scan() {
+		currCase := caseScanner.Bytes()
+		expectedText := expScanner.Text()
+		trunc := uint8(currCase[0])
+		truncText := Truncate(currCase[1:], int(trunc))
+		assert.Equal(t, expectedText, truncText)
+	}
+}
 
 func TestEncode(t *testing.T) {
 	testCases := []struct {
@@ -21,6 +40,8 @@ func TestEncode(t *testing.T) {
 		{0x085b, []byte{0xe0, 0xa1, 0x9b}},
 		{0x0c00, []byte{0xe0, 0xb0, 0x80}},
 		{0x1f308, []byte{0xf0, 0x9f, 0x8c, 0x88}},
+		{0x1f96a, []byte{0xf0, 0x9f, 0xa5, 0xaa}},
+		{0x1000ff, []byte{0xf4, 0x80, 0x83, 0xbf}},
 	}
 
 	for _, tc := range testCases {
--- bits-and-bytes/utf8-truncate/truncate.go
+++ bits-and-bytes/utf8-truncate/truncate.go
@@ -1,20 +1,77 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
-	// sandwich := 0x1F96A
-	num, _ := strconv.ParseInt(os.Args[1], 0, 32)
+	cases, err := os.Open("./cases")
+	must(err)
+	expected, err := os.Open("./expected")
+	caseScanner := bufio.NewScanner(cases)
+	expScanner := bufio.NewScanner(expected)
 
-	bytes := Encode(uint64(num))
-	for _, b := range bytes {
-		fmt.Printf("%x ", b)
+	for caseScanner.Scan() && expScanner.Scan() {
+		currCase := caseScanner.Bytes()
+		currExp := expScanner.Text()
+		trunc := uint8(currCase[0])
+		text := string(currCase[1:])
+		truncText := Truncate(currCase[1:], int(trunc))
+		fmt.Printf("Input: '%s' will be trucated at %d bytes\n", text, trunc)
+		fmt.Printf("Expect: '%s'\n", currExp)
+		fmt.Printf("Output: '%s'\n", truncText)
+		fmt.Printf("Matches: %t\n-----------\n", truncText == currExp)
 	}
-	fmt.Print("\n")
+}
+
+func Truncate(b []byte, truncIdx int) string {
+	if int(truncIdx) >= len(b) {
+		return string(b)
+	}
+
+	for truncIdx >= 0 {
+		// If this is not the start of UTF-8 or this is a zero-width-joiner, keep going backwards
+		if !isStartingByte(b[truncIdx]) || (truncIdx < len(b)-3 && isZWJ(b[truncIdx:truncIdx+3])) {
+			truncIdx--
+			continue
+		}
+
+		// If the codepoint before this is a ZWJ, skip it
+		if truncIdx >= 3 && isZWJ(b[truncIdx-3:truncIdx]) {
+			truncIdx -= 3
+			continue
+		}
+
+		// This is a starting byte that is not a ZWJ, and not preceded by a ZWJ
+		break
+	}
+
+	return string(b[:truncIdx])
+}
+
+func isZWJ(b []byte) bool {
+	return b[0] == 0xe2 && b[1] == 0x80 && b[2] == 0x8d
+}
+
+// Starting bytes begin with 00, 01, or 11
+func isStartingByte(b byte) bool {
+	return b&0xc0 != 0x80
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+func must(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
 
 func Encode(number uint64) []byte {
@@ -36,7 +93,7 @@ func Encode(number uint64) []byte {
 			byte(number>>18) | 0xf0,
 			byte(number>>12&0x3f) | 0x80,
 			byte(number>>6&0x3f) | 0x80,
-			byte(number&0x3f&0x3f) | 0x80,
+			byte(number&0x3f) | 0x80,
 		}
 	}
 
