ec633e9528a22428b545778377e43b38526dd20b
Josh Keller <jrkeller@gmail.com>
2023-06-30 07:07:38 -0400

    Simplify files

--- intro-to-network/http-header-server/main.go
+++ intro-to-network/http-header-server/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net"
+	"regexp"
+	"strings"
 )
 
 func main() {
@@ -102,4 +105,38 @@ func respondInternalError(conn net.Conn, err error) {
 	body, _ := json.Marshal(struct{ Error string }{Error: err.Error()})
 	respond(conn, "HTTP/1.1 500 Internal Server Error", body)
 	return
+}
+
+type HTTPRequest struct {
+	Method  string
+	Path    string
+	Version string
+	Headers map[string]string
+	Body    string
+}
+
+func ParseRequest(raw []byte) (*HTTPRequest, error) {
+	reqLineRE := regexp.MustCompile(`(?ms)^\s*(\w+)\s+([\w.\-\/]+)\s+(HTTP\/\d\.\d)\s*\r\n(.*)\r\n\r\n(.*)`)
+	match := reqLineRE.FindSubmatch(raw)
+	if len(match) == 0 {
+		return nil, errors.New("No match")
+	}
+	return &HTTPRequest{
+		Method:  string(match[1]),
+		Path:    string(match[2]),
+		Version: string(match[3]),
+		Headers: parseHeaders(string(match[4])),
+		Body:    string(match[5]),
+	}, nil
+}
+
+func parseHeaders(raw string) map[string]string {
+	headerRE := regexp.MustCompile(`(?mi)^\s*([\w\-]+):\s+(.*)$`)
+	matches := headerRE.FindAllStringSubmatch(raw, -1)
+	headers := make(map[string]string)
+	for _, match := range matches {
+		headers[match[1]] = strings.TrimSpace(match[2])
+	}
+
+	return headers
 }
