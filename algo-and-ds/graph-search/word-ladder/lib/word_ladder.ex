6c9b1a9fe91a52edb62b842b0416394edacaba39
Josh Keller <jrkeller@gmail.com>
2024-02-19 16:35:10 -0500

    Clean up

--- algo-and-ds/graph-search/word-ladder/lib/word_ladder.ex
+++ algo-and-ds/graph-search/word-ladder/lib/word_ladder.ex
@@ -1,25 +1,34 @@
 defmodule WordLadder do
   @wordlist File.read!("words.txt") |> String.upcase() |> String.split("\n", trim: true) |> MapSet.new()
 
+  def process_words do
+    result = case System.argv() do
+      [_, word1, word2 | _rest] ->
+        search(word1, word2)
+
+      _ ->
+        word1 = IO.gets("Enter the starting word: ") |> String.trim() |> String.upcase()
+        word2 = IO.gets("Enter the goal word: ") |> String.trim() |> String.upcase()
+        search(word1, word2)
+    end
+    
+    result
+    |> Enum.each(&IO.puts/1)
+  end
+
   def search(start, target) do
     _search(target, [{[], [start]}], MapSet.new(), [], MapSet.new())
   end
 
-  # defp _search(target, to_search, visited, next_to_search, next_visited)
   defp _search(target, [], visited, next_to_search, next_visited) do
     _search(target, next_to_search, MapSet.union(visited, next_visited), [], MapSet.new())
   end
 
   defp _search(target, [{history, words} | rest], visited, next_to_search, next_visited) do
-    IO.inspect(history)
-    IO.inspect(words)
-    IO.inspect(rest)
-    IO.puts("-------")
     if Enum.member?(words, target) do
       [target | history] |> Enum.reverse()
     else
       new_neighbors = all_neighbors({history, words}, visited)
-      IO.inspect(new_neighbors)
       _search(target, rest, visited, next_to_search ++ new_neighbors, MapSet.union(next_visited, visited_from_neigh(new_neighbors)))
     end
   end
@@ -31,9 +40,7 @@ defmodule WordLadder do
   end
   
   defp all_neighbors({history, words}, visited) do
-    IO.puts("alln")
     words
-    |> IO.inspect
     |> Enum.map(fn word -> 
       {[word | history], neighbors(word) |> Enum.filter(&(!MapSet.member?(visited, &1)))}
     end)
@@ -55,4 +62,4 @@ defmodule WordLadder do
   end
 end
 
-WordLadder.search("HEAD", "TAIL") |> IO.inspect
+WordLadder.process_words()
