c393f40fcdd71dbf152e99a5bf8b2f5d19af864e
Josh Keller <jrkeller@gmail.com>
2024-06-02 21:47:29 -0400

    Finish so

--- operating-systems/intro/stack-overflow/so.c
+++ operating-systems/intro/stack-overflow/so.c
@@ -9,23 +9,21 @@ void f(int x, long p) {
   f(x+1, p);
 }
 
-void start() {
-  int x = 0;
-  f(x, (long)&x);
-}
-
 int main(int argc, char *argv[])
 {
   long page_size = sysconf(_SC_PAGESIZE);
   printf("Page size: %ld\n", page_size);
+
   struct rlimit limit;
   getrlimit(RLIMIT_STACK, &limit);
-  printf("Limits: %lu, %luM\n", limit.rlim_cur, limit.rlim_max / (1024 * 1024));
+  printf("Initial Limits: %lu, %luM\n", limit.rlim_cur, limit.rlim_max / (1024 * 1024));
+
   limit.rlim_cur = 0;
   setrlimit(RLIMIT_STACK, &limit);
   getrlimit(RLIMIT_STACK, &limit);
-  printf("Limits: %lu, %luM\n", limit.rlim_cur, limit.rlim_max / (1024 * 1024));
-  start();
+  printf("Changed Limits: %lu, %luM\n", limit.rlim_cur, limit.rlim_max / (1024 * 1024));
+
+  f(0, (long)&argc);
   
   return EXIT_SUCCESS;
 }
