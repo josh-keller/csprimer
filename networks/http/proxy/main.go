ad7c24a2591e0e7f8c2ea546a0fa616710ce178c
Josh Keller <jrkeller@gmail.com>
2026-02-20 17:22:34 -0500

    http proxy clean up

--- networks/http/proxy/main.go
+++ networks/http/proxy/main.go
@@ -10,17 +10,6 @@ import (
 	"os"
 )
 
-// TODO:
-// - Send whole request to the upstream
-// - Don't block waiting for EOF
-
-// Use this for now to go fast and declutter code
-
-/*
-Receiving 400 from upstream.
-- Tried changing Host
--
-*/
 func must[T any](val T, err error) T {
 	if err != nil {
 		panic(err)
@@ -34,7 +23,7 @@ var (
 	response502 = []byte("HTTP/1.1 502 Bad gateway\r\n\r\n")
 )
 
-func print_err(format string, args ...any) {
+func printErr(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
 }
 
@@ -43,41 +32,39 @@ func handleConn(clientConn net.Conn) {
 
 	clientBuffer := make([]byte, 4096)
 	n, err := clientConn.Read(clientBuffer)
-	print_err("Received %d bytes from client", n)
+	if err != nil {
+		printErr("Error reading from client: %v", err)
+		return
+	}
+	printErr("C --> P     U (%d bytes)", n)
 
 	// For now, only check that it ends with \r\n\r\n.
 	// TODO: handle requests that come in more than one packet
-	if bytes.IndexAny(clientBuffer, "\r\n\r\n") == -1 {
+	if !bytes.ContainsAny(clientBuffer, "\r\n\r\n") {
 		clientConn.Write(response500)
+		printErr("Error reading from client: %v", err)
 		panic("ERROR requests over 4k not implemented")
 	}
 
 	// Connect upstream and make request
-	upstreamAddr := "127.0.0.1:8080"
+	upstreamAddr := "127.0.0.1:8000"
 	upstreamConn, err := net.Dial("tcp4", upstreamAddr)
 	if err != nil {
-		print_err("ERROR connecting upstream: %s", err)
+		printErr("ERROR connecting upstream: %s", err)
 		clientConn.Write(response502)
 		return
 	}
 	defer upstreamConn.Close()
 
-	// Get just the request line and write it upstream
-	// TODO: figure out why upstream wasn't working with full request
-	// requestLine := bytes.Split(clientBuffer, []byte("\r\n"))[0]
-	// requestLine = append(requestLine, []byte("\r\n\r\n")...)
-	// re := regexp.MustCompile(`Host: .*`)
-	// print_err("Raw request:\n%s", clientBuffer)
-	// upstreamRequest := re.ReplaceAllLiteral(clientBuffer, []byte("Host: "+upstreamConn.RemoteAddr().String()))
-	// print_err("Rewritten request:\n%s", upstreamRequest)
 	upstreamRequest := clientBuffer
 
 	n, err = upstreamConn.Write(upstreamRequest)
 	if err != nil {
 		clientConn.Write(response500)
-		print_err("ERROR writing upstream: %s", err)
+		printErr("ERROR writing upstream: %s", err)
 		return
 	}
+	printErr("C     P --> U (%d bytes)", n)
 
 	// Loop to get upstream response
 	totalBytesReturned := 0
@@ -87,41 +74,45 @@ func handleConn(clientConn net.Conn) {
 		// Theory is that the connection doesn't get EOF because browser is using keep-alive
 		// Eventually the browser closes and this call returns
 		n, err = upstreamConn.Read(upstreamBuffer)
-		print_err("Upstream buffer: %s", upstreamBuffer)
 		totalBytesReturned += n
 		if errors.Is(err, io.EOF) {
 			break
 		}
 		if err != nil {
-			print_err("ERROR: reading from upstream: %s", err)
+			printErr("ERROR: reading from upstream: %s", err)
 			clientConn.Write(response500)
 			break
 		}
-		_, err = clientConn.Write(upstreamBuffer)
+		printErr("C     P <-- U (%d bytes)", n)
+		_, err = clientConn.Write(upstreamBuffer[:n])
 		if err != nil {
-			print_err("ERROR: writing to client: %s", err)
+			printErr("ERROR: writing to client: %s", err)
 			break
 		}
+		printErr("C <-- P     U (%d bytes)", n)
 	}
-	print_err("%s <-- %s: %d bytes transferred", clientConn.RemoteAddr(), upstreamConn.RemoteAddr(), totalBytesReturned)
+	printErr("Client=%s, Upstream=%s, bytes=%d", clientConn.RemoteAddr(), upstreamConn.RemoteAddr(), totalBytesReturned)
 }
 
 func main() {
 	port := 4444
 	// Listen for connections
-	listenAddr := must(netip.ParseAddrPort(fmt.Sprintf("127.0.0.1:%d", port)))
-	listener := must(net.ListenTCP("tcp4", net.TCPAddrFromAddrPort(listenAddr)))
-	print_err("Listening for connections at %s...", listenAddr)
+	listenAddr, _ := netip.ParseAddrPort(fmt.Sprintf("127.0.0.1:%d", port))
+	listener, err := net.ListenTCP("tcp4", net.TCPAddrFromAddrPort(listenAddr))
+	if err != nil {
+		printErr("%v", err)
+		os.Exit(1)
+	}
+	defer listener.Close()
+	printErr("Listening for connections at %s...", listenAddr)
 	for {
 		clientConn, err := listener.Accept()
 		if err != nil {
-			print_err("Error accepting connection: %s", err)
+			printErr("Error accepting connection: %s", err)
 			continue
 		}
-		print_err("Connection received from %v", clientConn.RemoteAddr())
+		printErr("Connection received from %v", clientConn.RemoteAddr())
 
 		go handleConn(clientConn)
 	}
-
-	listener.Close()
 }
